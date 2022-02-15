77d165d3330557074c5db8e2798c0fa7a250e706
u76ner <muramatu.kosuke.0610@gmail.com>
2022-02-15 20:00:56 +0900

    push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSpeechSynthesis } from 'react-speech-kit';
 
 function App() {
+  const { speak } = useSpeechSynthesis();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => speak({ text: "blend em hitots" })}>ブレンドM1つ</button>
     </div>
   );
 }
